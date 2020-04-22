bc0d950877a1d2c18904ca6862a3eb86128af30d
mig82 <miguelangelxfm@gmail.com>
2020-04-22 17:37:28 +0200

    Test HelloWorld service

--- src/test/java/com/acme/services/HelloWorldTest.java
+++ src/test/java/com/acme/services/HelloWorldTest.java
@@ -1,7 +1,12 @@
 package com.acme.services;
 
 import com.konylabs.middleware.controller.DataControllerRequest;
+import com.konylabs.middleware.controller.DataControllerResponse;
+import com.konylabs.middleware.dataobject.Result;
 
+import java.util.HashMap;
+
+import static org.mockito.Mockito.*;
 import static org.testng.Assert.*;
 
 public class HelloWorldTest {
@@ -16,6 +21,21 @@ public class HelloWorldTest {
 
 	@org.testng.annotations.Test
 	public void testInvoke() throws Exception {
-		
+
+		//Mock the request.
+		DataControllerRequest request = mock(DataControllerRequest.class);
+		HashMap<String, Object> headers = (HashMap<String, Object>)request.getHeaderMap();
+
+		//Mock the response.
+		DataControllerResponse response = mock(DataControllerResponse.class);
+
+		HashMap<String, Object> cfgMap = new HashMap <String, Object>(); //Configurations.
+		HashMap<String, Object> inMap = new HashMap <String, Object>(); //Input parameters.
+		HashMap<String, Object> outMap = new HashMap <String, Object>(); //Output parameters.
+		Object[] maps = new Object[]{cfgMap, inMap, outMap};
+
+		HelloWorld hw = new HelloWorld();
+		Result result = hw.invoke("doFoo", maps, request, response);
+		assertEquals(result.getParamByName("message").getValue(), "Hello World!");
 	}
 }
